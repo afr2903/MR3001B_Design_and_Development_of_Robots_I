567e8ddfefaac7df1f445d28a0e4c3fec79f6eea
afr2903 <afr102903@gmail.com>
2024-03-24 21:53:43 -0600

    Refactored code main logic

--- class_ws/src/closed_loop_control/scripts/closed_loop.py
+++ class_ws/src/closed_loop_control/scripts/closed_loop.py
@@ -20,18 +20,14 @@ class Puzzlebot:
         self.wheel_base = 0.19
 
         # Variables/Parameters to be used
-        self.target_iterations = 4
+        self.target_path = [
+            [3.3,  0  ],
+            [3.85, 3.1],
+            [0.7,  4.4],
+            [0.6,  0.7]
+        ]
 
         # Closed Loop
-        self.distance_step = -1300
-        self.angle_step = 7.67
-        self.move_angle_step = 7.67
-
-        self.target_distance = 4500
-        self.target_angle = 0
-        self.move_angle = 0 # radians
-        self.distance_tolerance = 5
-        self.angle_tolerance = 0.05
 
         # Kinematics
         self.right_wheel = 0.0
@@ -40,30 +36,25 @@ class Puzzlebot:
         self.angular_velocity = 0.0
 
         self.distance = 0.0
+        self.x = 0.0
+        self.y = 0.0
         self.angle = 0.0
-        self.imu_angle = 0.0
         self.last_time = 0.0
 
-        self.task_states = {
-            "IDLE": 0,
-            "TURN": 1,
-            "MOVE": 2
-        }
-        self.current_task = self.task_states["IDLE"]
         self.current_iteration = 0
 
         # PID Parameters
         self.rotation_pid = {
-            "kp": 0.015,
-            "ki": 0.0005,
-            "kd": 0.0001,
+            "kp": 0.8,
+            "ki": 0.00001,
+            "kd": 0.0,
             "integral": 0.0,
             "last_error": 0.0
         }
 
         self.translation_pid = {
-            "kp": 0.2,
-            "ki": 0.0,
+            "kp": 0.1,
+            "ki": 0.00001,
             "kd": 0.0,
             "integral": 0.0,
             "last_error": 0.0
@@ -101,7 +92,7 @@ class Puzzlebot:
         self.rotation_pid["integral"] += error * dt
         derivative = (error - self.rotation_pid["last_error"]) / dt
         self.rotation_pid["last_error"] = error
-        return self.rotation_pid["kp"] * error + self.rotation_pid["ki"] * self.rotation_pid["integral"] - self.rotation_pid["kd"] * derivative
+        return self.rotation_pid["kp"] * error - self.rotation_pid["ki"] * self.rotation_pid["integral"] - self.rotation_pid["kd"] * derivative
     
     def translation_control(self, error, dt):
         self.translation_pid["integral"] += error
@@ -128,7 +119,7 @@ class Puzzlebot:
             self.last_time = rospy.Time.now().to_sec()
             return
 
-        self.set_twist(0.0, self.rotation_control(self.target_angle - self.angle, dt))
+        self.set_twist(0.0, 0.1*self.rotation_control(self.target_angle - self.angle, dt))
 
     """
     Open-loop control for movement
@@ -166,30 +157,52 @@ class Puzzlebot:
         # Linear and angular velocity calculation with basic kinematics
         self.loop_rate.sleep()
 
-        self.linear_velocity = self.wheel_radius * (self.right_wheel + self.left_wheel) / 2
-        self.angular_velocity = self.wheel_radius * (self.right_wheel - self.left_wheel) / (self.wheel_base) * math.pi / 180
-
         dt = rospy.Time.now().to_sec() - self.last_time
         if dt < self.sample_time:
             return
-
-        if self.current_task == self.task_states["IDLE"]:
-            self.stop()
-            if self.current_iteration == self.target_iterations:
-                rospy.loginfo("Open-loop routine complete")
-                rospy.signal_shutdown("Routine complete")
-            else:
-                #self.target_distance += self.distance_step
-                #self.target_angle += self.angle_step
-                #self.move_angle += self.move_angle_step
-                self.current_task = self.task_states["MOVE"]
-                self.last_time = rospy.Time.now().to_sec()
         
-        elif self.current_task == self.task_states["TURN"]:
-            self.rotate(dt)
+        self.x += self.wheel_radius * (self.right_wheel + self.left_wheel)/2 * math.cos(self.angle) * dt
+        self.y += self.wheel_radius * (self.right_wheel + self.left_wheel)/2 * math.sin(self.angle) * dt
+        self.angle += self.wheel_radius * (self.right_wheel - self.left_wheel) / self.wheel_base * dt
 
-        elif self.current_task == self.task_states["MOVE"]:
-            self.move(dt)        
+        #rospy.loginfo(f"X: {self.x}, Y: {self.y}, T: {self.angle}")
+        # print with 2 decimal places
+        rospy.loginfo(f"X: {self.x:.2f}, Y: {self.y:.2f}, T: {self.angle:.2f}")
+
+        required_angle = math.atan2(self.target_path[self.current_iteration][1] - self.y, self.target_path[self.current_iteration][0] - self.x)
+        if required_angle < -.1:
+            required_angle += 2*math.pi
+        self.error_angle = required_angle - self.angle
+        self.error_distance = math.sqrt((self.target_path[self.current_iteration][0] - self.x)**2 + (self.target_path[self.current_iteration][1] - self.y)**2)
+
+        if abs(self.error_angle) < 0.13 and abs(self.error_distance) < 0.25:
+            self.stop()
+            rospy.loginfo("Reached target")
+            rospy.sleep(3.0)
+            self.current_iteration += 1
+            #self.x = self.target_path[self.current_iteration][0]
+            self.translation_pid["integral"] = 0.0
+            self.rotation_pid["integral"] = 0.0
+        
+        v = self.translation_control(self.error_distance, dt)
+        w = self.rotation_control(self.error_angle, dt)
+
+        if abs(self.error_angle) > 0.8:
+            v = 0.0
+
+        self.set_twist(v, w)
+
+        #rospy.loginfo(f"V: {v}, W: {w}")
+        rospy.loginfo(f"EA: {self.error_angle:.2f}, ED: {self.error_distance:.2f}")
+
+        self.last_time = rospy.Time.now().to_sec()
+
+        if self.current_iteration >= len(self.target_path):
+            self.stop()
+            rospy.loginfo("Path completed")
+            rospy.signal_shutdown("Path completed")
+            return
+
 
 
 if __name__ == "__main__":
